6ce87c15270f6d8786782bcdf987f6324c3d890a
deeps121890 <deepikadeepu8818@gmail.com>
2024-10-03 02:00:42 -0700

    Update x,y.py

--- x,y.py
+++ x,y.py
@@ -12,3 +12,19 @@ Sample Input:
 Sample Output:
 6 less than 8
 '''
+# Function to compare two integers
+def compare_numbers(x, y):
+    if x == y:
+        return f"{x} and {y} are equal"
+    elif x > y:
+        return f"{x} greater than {y}"
+    else:
+        return f"{x} less than {y}"
+
+# Input reading
+a = int(input())  # First integer
+b = int(input())  # Second integer
+
+# Compare and print the result
+result = compare_numbers(a, b)
+print(result)
