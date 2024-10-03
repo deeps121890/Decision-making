fe6c8fec48729a9948d7411cbf3eeff10cc0d29b
deeps121890 <deepikadeepu8818@gmail.com>
2024-10-03 01:57:26 -0700

    Update cse.py

--- cse.py
+++ cse.py
@@ -15,3 +15,24 @@ Sample Input:
 Sample Output:
 L3
 '''
+# Function to find the lab with minimal seating capacity
+def find_min_capacity_lab(capacities):
+    # Find the index of the minimum capacity
+    min_index = capacities.index(min(capacities))
+    
+    # Lab names corresponding to the indices
+    lab_names = ['L1', 'L2', 'L3']
+    
+    return lab_names[min_index]
+
+# Input reading
+a = int(input())  # Seating capacity of L1
+b = int(input())  # Seating capacity of L2
+c = int(input())  # Seating capacity of L3
+
+# List of lab capacities
+capacities = [a, b, c]
+
+# Find and print the lab with minimal seating capacity
+min_lab = find_min_capacity_lab(capacities)
+print(min_lab)
